bc937d49d9bddc0b4ca1f3913a4ea6a58b1db732
AugustoDiaz-Dev <augustodiaz_1@hotmail.com>
2021-08-03 00:10:03 +1000

    Modules

---
+++ Python-Course/Modules_first_approach/app.py
@@ -0,0 +1,9 @@
+#Topic: Import a function from a created module.
+import useful_tools
+
+print(useful_tools.meters_in_kilometer * 15, "meters.")
+
+print(useful_tools.beatles[0])
+
+print(useful_tools.roll_dice(6))
+
