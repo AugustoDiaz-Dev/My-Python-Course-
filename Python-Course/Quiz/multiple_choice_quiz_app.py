2fba83b17f15743fee9e998bbd8d0ecc4575df46
AugustoDiaz-Dev <augustodiaz_1@hotmail.com>
2021-08-04 00:03:11 +1000

    Quiz

---
+++ Python-Course/Quiz/multiple_choice_quiz_app.py
@@ -0,0 +1,26 @@
+#Topic: import a class.
+
+from question import Question
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Organge\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer: 
+            score += 1
+            
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct.")
+
+run_test(questions)
