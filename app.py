7c10c0a3c6e9471cf9d21aebc6418d287bf44308
Augusto <85144745+AugustoDiaz-Dev@users.noreply.github.com>
2022-09-06 00:59:41 +1000

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+# print method
+print('Hi Augusto!')
+# Python code gets executed line by line from top to bottom
+print('O----')
+print('||||')
+# Types of data
+# This is called "an expression". An expression is a piece of code that produces a value.
+print('*' * 10)
+# TASK 1 Try to draw another shape or letter, or animal.
+print('-----')
+print('  |  ')
+print('  |  ')
+print('  |  ')
