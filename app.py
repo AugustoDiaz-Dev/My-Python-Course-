0a9a91b9e5854e55b4cb6cc49b95350f0d24ad6f
Augusto <85144745+AugustoDiaz-Dev@users.noreply.github.com>
2022-09-06 01:23:37 +1000

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ print('-----')
 print('  |  ')
 print('  |  ')
 print('  |  ')
+
+# Variables (identifier, assignation, value)
+price = 10
+price = 20
+print(price)
+# Integers and floats
+apples = 5
+rating = 4.5
+name = 'Augusto'
+is_published = True
+print(rating)
+# Python is a case-sensitive language. When defining variables we always have to use lower case (snake case).
+# Task 2 - At School. We have a student called George Harrison. He's 8 years old and is a new student.
+# Define three variables: age, full_name and is_new_student.
+full_name = 'George Harrison'
+age = 20
+is_new_student = True
