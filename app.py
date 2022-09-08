b2f04b4b9002d496ebcda7ee906ec06242483b8e
Augusto <85144745+AugustoDiaz-Dev@users.noreply.github.com>
2022-09-08 11:52:39 +1000

    Update app.py

--- app.py
+++ app.py
@@ -140,5 +140,31 @@ print(math.floor(x))
 
 # check out the docs for more methods: https://docs.python.org/3/library/math.html
 
+# Conditions
+is_hot = False
+is_cold = False
+if is_hot:
+    print("It's a hot day.")
+    print("Drink plenty of water.")
+elif is_cold:
+    print("It's a cold day.")
+    print("Wear warm clothes.")
+else:
+    print("It's a lovely day!")
+# Task 6.
+# Price of a house is $ 1 million.
+# If buyer has good credit,
+#   they need to put down 10%
+# otherwise
+#   they need to put down 20%
+# print the down payment
+price = 1000000
+has_good_credit = False
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"Down payment: $ {down_payment}")
+
 
 # useful python modules: Math
