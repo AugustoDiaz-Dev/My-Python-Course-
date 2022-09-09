2cef17d5ea41af19e6ec78b4e17d35b03523c8eb
Augusto <85144745+AugustoDiaz-Dev@users.noreply.github.com>
2022-09-09 16:31:45 +1000

    Update app.py

--- app.py
+++ app.py
@@ -190,5 +190,34 @@ if has_good_credit and not has_criminal_record:
 else:
     print('Not eligible')
     
+# Comparison operators - < > <= >= == !=
+# if temperature is greater than 30
+#   it's a hot day
+# otherwise if it's less than 10
+#   it's a cold day
+# otherwise
+#   it's neither hot nor cold
+temperature = 9
+if temperature > 30:
+    print("It's a hot day")
+elif temperature < 10:
+    print("it's a cold day")
+else:
+    print("It's neither hot nor cold")
 
+# Task 7
+# if name is less than 3 characters long
+#   name must be at least 3 characters
+# otherwise if it's more than 50 characters long
+#   name must be a maximum of 50 characters
+# otherwise
+#   name looks good!
+name = 51
+if name < 3:
+    print("Name must be at least 3 characters")
+elif name >= 50:
+    print("Name must be a maximum of 50 characters")
+else:
+    print("Name looks good!")
+    
 # useful python modules: Math
