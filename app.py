6e94baf99e9b2ef2427206313e0dc26831e816c3
Augusto <85144745+AugustoDiaz-Dev@users.noreply.github.com>
2022-09-11 21:16:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -245,5 +245,17 @@ while i <= 5:
     i = i + 1
 print("Done!")
 
+# Guessing Game
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print('You Won!')
+        break
+else:
+    print('Sorry, you failed.')
 
 # useful python modules: Math
