3913cea4d7d8fa7ee95c939d52c617f2bee8e7a3
Augusto <85144745+AugustoDiaz-Dev@users.noreply.github.com>
2022-09-14 00:06:48 +1000

    Update app.py

--- app.py
+++ app.py
@@ -343,3 +343,23 @@ for number in numbers_list:
     if number > max:
         max = number
 print(max)
+
+# 2D lists
+'''
+[
+    1 2 3
+    4 5 6
+    7 8 9
+]
+3x3
+'''
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+matrix[0][1] = 20
+print(matrix[0][1])
+for row in matrix:
+    for item in row:
+        print(item)
