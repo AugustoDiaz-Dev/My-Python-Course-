a83fbefed4073dea8790906fcad6f1bc6bbf7094
Augusto <85144745+AugustoDiaz-Dev@users.noreply.github.com>
2022-09-17 18:33:16 +1000

    Update app.py

--- app.py
+++ app.py
@@ -561,3 +561,20 @@ class Person:
 
 augusto = Person('Augusto Diaz')
 augusto.talk()
+
+# Inheritance
+class Mammal:
+    def walk(self):
+        print("Walk")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print('bark')
+
+
+class Cat(Mammal):
+    pass
+
+dog1 = Dog()
+dog1.walk()
